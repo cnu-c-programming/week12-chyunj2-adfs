d668e1523c18bf0fa342b36479548f66298763cf
chyunj2-adfs <chyunj2@gmail.com>
2026-05-28 21:28:51 +0900

    Update 2-4.c

--- code/2-4.c
+++ code/2-4.c
@@ -6,7 +6,16 @@ int main(int argc, const char* argv[]) {
 
     FILE* fp = fopen(argv[1], "r");
 
-
+int a[26] = {0};
+    char c;
+    while((c = fgetc(fp)) != EOF) {
+        if (c >= 'a' && c <= 'z') {
+            a[c - 'a']++;
+        }
+    }
+    for(int i = 0; i < 26; i++) {
+        printf("%c: %d\n", 'a' + i, a[i]);
+    }
 
     fclose(fp);
 }
