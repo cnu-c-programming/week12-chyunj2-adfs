2506e6a477675ee3ef7c3682466a955bf8758ff0
chyunj2-adfs <chyunj2@gmail.com>
2026-05-28 23:37:55 +0900

    Update 3-5.c

--- code/3-5.c
+++ code/3-5.c
@@ -11,7 +11,25 @@ int main(int argc, const char* argv[]) {
 
     int sum = 0;
 
-
+char a[100];
+    while(fscanf(fp, "%s", a) == 1) {
+        int i = 0;
+        int num = 0;
+        int b = 1; 
+        while(a[i]  != '\0') {
+            if (a[i] < '0' || a[i] > '9') {
+                b = 0;
+                break;
+            }
+            num = num * 10 + (a[i] - '0');
+            i++;
+        }
+        if(b == 1) {
+            sum += num;
+        } else{
+            fprintf(stderr, "invalid input %s\n", a);
+        }
+    }
     printf("sum: %d\n", sum);
     fclose(fp);
     return 0;
