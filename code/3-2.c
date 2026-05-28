d6e7ea932093a8dc2b8f7f9f98897b38da646eb0
chyunj2-adfs <chyunj2@gmail.com>
2026-05-28 23:17:45 +0900

    Update 3-2.c

--- code/3-2.c
+++ code/3-2.c
@@ -10,7 +10,11 @@ int main(int argc, const char* argv[]) {
 
     FILE* fp = fopen(filename, "r");
 
-
+   if(fp == NULL){
+        return 0;
+    }
+    fscanf(fp, "%s", buffer);
+    fclose(fp);
     return 0;
 }
 
