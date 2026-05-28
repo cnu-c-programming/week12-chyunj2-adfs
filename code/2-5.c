15b175145f8bfcea7f4edebaf3a9c38b9e34fc8b
chyunj2-adfs <chyunj2@gmail.com>
2026-05-28 22:27:16 +0900

    Update 2-5.c

--- code/2-5.c
+++ code/2-5.c
@@ -3,10 +3,13 @@
 int main(int argc, const char* argv[]) {
     if (argc < 3)
         return 0;
+    FILE* fp_src = fopen(argv[1], "r");
+    FILE* fp_dst = fopen(argv[2], "w");
 char c[1024];
     while (fgets(c, sizeof(c), fp_src) != NULL) {
         fputs(c, fp_dst);
     }
-
+ fclose(fp_src);
+    fclose(fp_dst);
 }
 
